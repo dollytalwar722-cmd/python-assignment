869d27c957a1f699b1179bf366fdf88c1f07a040
kashish <dollytalwar722@gmail.com>
2026-03-17 11:21:53 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,53 @@
+
+#program to write hello python
+print ("hello python")
+
+# program to write your name as input and print hello
+name = input("enter your name:")
+print("hello",name)
+
+#program to take one integer input and print it
+number= int(input("enter an integer :"))
+print(number)
+
+# program to take two integers and print sum
+a= int(input("enter an integer:"))
+b=int(input("enter an integer:"))
+sum=a+b
+print("sum",sum)
+
+# program to take  two integer and  print their multiplication
+a= int(input("enter a number:"))
+b=int(input("enter a number:"))
+product=a*b
+print("product",product)
+
+#program to take one float number and print
+number=float(input("enter a number:"))
+print ("  floatnumber", number)
+
+#program to take two float number and print their average
+a=float(input("enter a number:"))
+b=float(input("enter a number:"))
+average=(a+b)/2
+print("average",average)
+#program to calculate area of rectangle
+length=float(input("enter a length:"))
+breadth=float(input("enter a breadth:"))
+area=length*breadth
+print("areaofrectangle",area)
+#program to conver integer to float
+# doubt
+
+#program to take price pf three items and print total price
+
+price1 = float(input("enter price of item 1:"))
+price2=float(input("enter price of item 2:"))
+price3= float(input("enter price of item 3:"))
+sum=price1+price2+price3
+print(" totalprice",sum)
+             
+
+
+
+
